402006c9b9984e2d54eee7d7a23523ddc5de748a
Jordan Grubb <imjordangrubb@gmail.com>
2021-01-11 18:41:15 -0500

    changes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}.`);
-})
+});
