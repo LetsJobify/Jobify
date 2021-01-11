e8f2cc47fdde077e5e35475968883aa157694499
Warren Tait <warrenhtait@gmail.com>
2021-01-11 15:19:19 -0500

    Set up React with Webpack

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
 const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}.`);
