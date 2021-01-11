7c1f17ddf54208447c5e5495eda664613c368bc9
Warren Tait <warrenhtait@gmail.com>
2021-01-11 14:53:02 -0500

    Instantiated webpack for project files.

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import App from './components/App';
+import './stylesheets/styles.scss';
+
+render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    port: 8080,
+    publicPath: '/build',
+    hot: true,
+    proxy: {
+      '/': 'http://localhost:3000/',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            "style-loader",
+            "css-loader",
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+    ]
+  }
+};
