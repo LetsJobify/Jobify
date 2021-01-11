71a5959f04b4257f4a491031218f4ae33781f52e
Warren Tait <warrenhtait@gmail.com>
2021-01-11 18:43:42 -0500

    Adjusted Express Router

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const router = express.Router;
+const router = express.Router();
 
 // user form - single user id (expect id in query param)
-router.get('/:id', (req,res)=> );
+router.get('/:id', (req,res)=> {return});
 
 // create user (expects all user info in body)
-router.post('/', (req,res)=>);
+router.post('/', (req,res)=> {return});
 
 // update user (expects all user info in body)
-router.put('/:id', (req,res)=>);
+router.put('/:id', (req,res)=> {return});
 
 // delete user (expect id in query param)
-router.delete('/:id', (req,res)=>);
+router.delete('/:id', (req,res)=> {return});
 
 module.exports = router;
