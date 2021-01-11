3e3973a95b6e608102856ea7b44bff93e90a7ea6
Warren Tait <warrenhtait@gmail.com>
2021-01-11 15:30:00 -0500

    Added light and dark mode for use throughout application.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { 
+  Button,
+  useColorMode,
+ } from '@chakra-ui/react';
+ import {
+   MoonIcon,
+   SunIcon
+ } from '@chakra-ui/icons';
 
 export default function App() {
+
+    // Importing Light and Dark mode for application.
+    const { colorMode, toggleColorMode } = useColorMode();
+
+
   return (
     <div>
-      HELLO!
+      <Button 
+        onClick={toggleColorMode}
+        className='offset'
+      >
+        {colorMode === 'dark' ? <SunIcon /> : <MoonIcon />}
+      </Button>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { 
+  ChakraProvider,
+  extendTheme
+ } from '@chakra-ui/react';
 import App from './components/App';
 import './stylesheets/styles.scss';
 
-render(<App />, document.getElementById('root'));
+// Set color theme throughout application.
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const theme = extendTheme({ colors });
+
+render(
+  <ChakraProvider theme={theme}>
+    <App />
+  </ChakraProvider>, 
+  document.getElementById('root')
+);
