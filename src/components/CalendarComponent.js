26f8fe60b63246547436b30384736170280a279c
Jordan Grubb <imjordangrubb@gmail.com>
2021-01-12 15:39:40 -0500

    calendar development

--- src/components/CalendarComponent.js
+++ src/components/CalendarComponent.js
@@ -1,24 +1,27 @@
-import React from 'react'
-import { FullCalendar } from '@fullcalendar/core';
+import React, { useState } from 'react';
+import Calendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
-// import timeGridPlugin from '@fullcalendar/timegrid';
-// import listPlugin from '@fullcalendar/list';
-
-// let calendar = new Calendar(calendarEl, {
-//     plugins: [ dayGridPlugin, timeGridPlugin, listPlugin ]
-//   });
-
-//   useEffect(() => {
-//       Calendar.render()
-//   },[]);
+import interactionPlugin from '@fullcalendar/interaction';
+import listPlugin from '@fullcalendar/list';
+// needed for dayClick
 
 export default function CalendarComponent() {
+  const handleDateClick = (arg) => {
+    alert(arg.dateStr);
+  };
   return (
-    <div>
-        <FullCalendar
-        defaultView="dayGridMonth" plugins={[ dayGridPlugin ]}
-        
-        />
+    <div className="calendar">
+      <Calendar
+        plugins={[dayGridPlugin, interactionPlugin, listPlugin]}
+        dateClick={handleDateClick}
+        initialView="dayGridMonth"
+        weekends={true}
+        events={[
+          { title: 'event 1', date: '2021-01-10' },
+          { title: 'event 2', date: '2021-01-12' },
+          { title: 'event 3', date: '2021-01-10' },
+        ]}
+      />
     </div>
-  )
+  );
 }
