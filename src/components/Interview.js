60e61b2aa145623980916b7623bb16f9b7a9844d
Warren Tait <warrenhtait@gmail.com>
2021-01-12 22:15:44 -0500

    Worked on styling, etc.

--- src/components/Interview.js
+++ src/components/Interview.js
@@ -32,151 +32,206 @@ export default function Interview() {
   const [ sliderValue, setSliderValue ] = useState(0);
   const handleSliderChange = (sliderValue) => setSliderValue(sliderValue);
 
+  // Show or hide the interview form.
+  const [ formVisibility, setFormVisibility ] = useState(false);
+
+  // Interview form object state.
+  const [ formState, setFormState ] = useState({
+    company: '',
+    date: '',
+    address: '',
+    notes: '',
+    feedback: '',
+    type: '',
+    rating: 0,
+    interviewer: '',
+    faq: '',
+    offer: 0,
+    accepted: ''
+  });
+
   return (
-    <Container 
-      bg={`${colorMode}.500`}
-      maxW="38vw"
-      borderWidth="1px"
-      borderRadius="lg"
-      overflow="hidden"
-      pt="4"
-      pr="4"
-      pb="4"
-      pl="4"
-      mt="6"
-    >
-      <div className='two_items'>
-        <span>
-          <Text mb="1">Company</Text>
-          <Select w="16vw">
-            <option value="google">Google</option>
-            <option value="google">Amazon</option>
-            <option value="google">Facebook</option>
-            <option value="google">Netflix</option>
-          </Select>
-        </span>
-        <span>
-          <Text mb="1">Date</Text>
-          <Input w="16vw" />
-        </span>
-      </div>
-      <div className='one_item'>
-        <span>
-          <Text mt="4">Company Address</Text>
-          <Input 
-            variant="flushed"
-            size="lg"
-            w="30vw"
-          />
-        </span>
-      </div>
-      <div className='one_item'>
-        <span>
-          <Text mt="3">Notes</Text>
-          <Textarea 
-            mt="1"
-            placeholder="Things to brush up on / goals / notes..."
-            resize="none"
-            w="30vw"
-            h="12vh"
-          />
-        </span>
-      </div>
-      <div className='one_item'>
-        <Text mt="2" mb="2">Post Interview</Text>
-      </div>
-      <div className='one_item'>
-        <span>
-          <Text mt="3">Feedback from Interview</Text>
-          <Textarea 
-            mt="1"
-            mb="3"
-            placeholder="Enter feedback here..."
-            resize="none"
-            w="30vw"
-            h="6vh"
-          />
-        </span>
-      </div>
-      <div className='three_items'>
-        <span>
-          <Text mb="1">Type</Text>
-          <Select w="8vw">
-            <option value="behavioral">Behavioral</option>
-            <option value="technical">Technical</option>
-          </Select>
-        </span>
-        <span>
-          <Text mb="1">Rating</Text>
-          <Flex>
-            <NumberInput maxW="75px" mr="2rem" min="0" max="100" value={sliderValue} onChange={handleSliderChange}>
+    <div>
+      { formVisibility ? 
+      <Container 
+        bg={`${colorMode}.500`}
+        maxW="38vw"
+        borderWidth="1px"
+        borderRadius="lg"
+        overflow="hidden"
+        pt="4"
+        pr="4"
+        pb="4"
+        pl="4"
+        mt="6"
+      >
+        <Center>
+          <Button
+            onClick={() => setFormVisibility(!formVisibility)}
+            w="38vw"
+            size="xs"
+            mb="4"
+          >
+            Hide
+          </Button>
+        </Center>
+        <div className='two_items'>
+          <span>
+            <Text mb="1">Company</Text>
+            <Select w="16vw">
+              <option value="google">Google</option>
+              <option value="google">Amazon</option>
+              <option value="google">Facebook</option>
+              <option value="google">Netflix</option>
+            </Select>
+          </span>
+          <span>
+            <Text mb="1">Date</Text>
+            <Input w="16vw" />
+          </span>
+        </div>
+        <div className='one_item'>
+          <span>
+            <Text mt="4">Company Address</Text>
+            <Input 
+              variant="flushed"
+              size="lg"
+              w="30vw"
+            />
+          </span>
+        </div>
+        <div className='one_item'>
+          <span>
+            <Text mt="3">Notes</Text>
+            <Textarea 
+              mt="1"
+              placeholder="Things to brush up on / goals / notes..."
+              resize="none"
+              w="30vw"
+              h="12vh"
+            />
+          </span>
+        </div>
+        <div className='one_item'>
+          <Text mt="2" mb="2">Post Interview</Text>
+        </div>
+        <div className='one_item'>
+          <span>
+            <Text mt="3">Feedback from Interview</Text>
+            <Textarea 
+              mt="1"
+              mb="3"
+              placeholder="Enter feedback here..."
+              resize="none"
+              w="30vw"
+              h="6vh"
+            />
+          </span>
+        </div>
+        <div className='three_items'>
+          <span>
+            <Text mb="1">Type</Text>
+            <Select w="8vw">
+              <option value="behavioral">Behavioral</option>
+              <option value="technical">Technical</option>
+            </Select>
+          </span>
+          <span>
+            <Text mb="1">Rating</Text>
+            <Flex>
+              <NumberInput maxW="75px" mr="2rem" min="0" max="100" value={sliderValue} onChange={handleSliderChange}>
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+              <Slider flex="1" w="5vw" value={sliderValue} onChange={handleSliderChange}>
+                <SliderTrack >
+                  <SliderFilledTrack />
+                </SliderTrack>
+                <SliderThumb fontSize="sm" boxSize="24px" children={sliderValue} />
+              </Slider>
+            </Flex>
+          </span>
+          <span>
+            <Text>Interviewer</Text>
+            <Input 
+              size="lg"
+              w="12vw"
+            />
+          </span>
+        </div>
+        <div className='one_item'>
+          <span>
+            <Text mt="3">Questions Asked on Interview</Text>
+            <Textarea 
+              mt="1"
+              mb="3"
+              placeholder="Enter any questions asked on the interview here..."
+              resize="none"
+              w="30vw"
+              h="6vh"
+            />
+          </span>
+        </div>
+        <div className='two_items'>
+          <span>
+            <Text mb="1">Offer Amount</Text>
+            <NumberInput allowMouseWheel w="16vh" defaultValue={80000} min={0}>
               <NumberInputField />
               <NumberInputStepper>
                 <NumberIncrementStepper />
                 <NumberDecrementStepper />
               </NumberInputStepper>
             </NumberInput>
-            <Slider flex="1" w="5vw" value={sliderValue} onChange={handleSliderChange}>
-              <SliderTrack >
-                <SliderFilledTrack />
-              </SliderTrack>
-              <SliderThumb fontSize="sm" boxSize="24px" children={sliderValue} />
-            </Slider>
-          </Flex>
-        </span>
-        <span>
-          <Text>Interviewer</Text>
-          <Input 
-            size="lg"
-            w="12vw"
-          />
-        </span>
-      </div>
-      <div className='one_item'>
-        <span>
-          <Text mt="3">Questions Asked on Interview</Text>
-          <Textarea 
-            mt="1"
-            mb="3"
-            placeholder="Enter any questions asked on the interview here..."
-            resize="none"
-            w="30vw"
-            h="6vh"
-          />
-        </span>
-      </div>
-      <div className='two_items'>
-        <span>
-          <Text mb="1">Offer Amount</Text>
-          <NumberInput allowMouseWheel w="16vh" defaultValue={80000} min={0}>
-            <NumberInputField />
-            <NumberInputStepper>
-              <NumberIncrementStepper />
-              <NumberDecrementStepper />
-            </NumberInputStepper>
-          </NumberInput>
-        </span>
-        <span>
-          <Text mb="1">Accepted Offer?</Text>
-          <Select w="16vw">
-            <option value="no">No</option>
-            <option value="yes">Yes</option>
-          </Select>
-        </span>
-      </div>
-      <div className='one_item'>
-        <span>
-          <Text mt="2" mb="5">Fool me once, shame on... shame on you. Fool me—you can't get fooled again.</Text>
-        </span>
-      </div>
-      <div className='two_buttons'>
-        <Button colorScheme="red">
-          Delete
-        </Button>
-        <Button colorScheme="green">
-          Save/Confirm
-        </Button>
-      </div>
-    </Container>
+          </span>
+          <span>
+            <Text mb="1">Accepted Offer?</Text>
+            <Select w="16vw">
+              <option value="no">No</option>
+              <option value="yes">Yes</option>
+            </Select>
+          </span>
+        </div>
+        <div className='one_item'>
+          <span>
+            <Text mt="2" mb="5">Fool me once, shame on... shame on you. Fool me—you can't get fooled again.</Text>
+          </span>
+        </div>
+        <div className='two_buttons'>
+          <Button colorScheme="red">
+            Delete
+          </Button>
+          <Button colorScheme="green">
+            Save/Confirm
+          </Button>
+        </div>
+      </Container>
+      :
+      <Container
+        bg={`${colorMode}.500`}
+        maxW="38vw"
+        borderWidth="1px"
+        borderRadius="lg"
+        overflow="hidden"
+        pt="4"
+        pr="4"
+        pb="4"
+        pl="4"
+        mt="6"
+      >
+        <Center>
+          <Button
+            onClick={() => setFormVisibility(!formVisibility)}
+            w="38vw"
+            size="xs"
+          >
+            Show Interview Form
+          </Button>
+        </Center>
+      </Container>
+      }
+    </div>
   )
 }
