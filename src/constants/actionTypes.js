a9027c9b261c61b42972a4a59736732f13fcedba
Warren Tait <warrenhtait@gmail.com>
2021-03-12 18:34:30 -0500

    Added action types.

---
+++ src/constants/actionTypes.js
@@ -0,0 +1,14 @@
+/**
+ * ************************************
+ *
+ * @module  actionTypes.js
+ * @author  Warren Tait
+ * @date
+ * @description Action Type Constants
+ *
+ * ************************************
+ */
+
+export const LOG_IN_USER = 'LOG_IN_USER';
+
+export const LOG_OUT_USER = 'LOG_OUT_USER';
